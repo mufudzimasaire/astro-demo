ad9e75bc593b53334512e163aa2d64f1ba3647c9
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2022-10-18 18:07:02 +0100

    Add initial netlify/functions/get-product

---
+++ netlify/functions/get-product.ts
@@ -0,0 +1,81 @@
+import type { Handler } from '@netlify/functions'
+const axios = require('axios');
+
+interface ShiftProduct {
+  id: string,
+  type: string,
+  attributes:  {
+    canonical_path: string
+    public_primary_asset_file_url: string
+    reference: string
+    title: string
+  },
+  relationships: any
+}
+
+export interface Product {
+  id: string
+  canonical_path: string
+  public_primary_asset_file_url: string
+  reference: string
+  title: string
+}
+
+/**
+ * Flattens API product attributes
+ */
+function transformProduct(product: ShiftProduct): Product[] {
+  return {
+    id: product?.id || '',
+    canonical_path: product?.attributes?.canonical_path || '',
+    public_primary_asset_file_url: product?.attributes?.public_primary_asset_file_url || '',
+    reference: product?.attributes?.reference || '',
+    title: product?.attributes?.title || '',
+  }
+}
+
+/**
+ * Product API Endpoint
+ *
+ * Purpose: Fetches Product from Platform API
+ *
+ * Example:
+ * ```
+ * fetch('/.netlify/functions/get-product?id=1')
+ * ```
+ */
+export const handler: Handler = async (event) => {
+  try {
+    const {
+      API_TENANT,
+      API_HOST,
+      API_ACCESS_TOKEN
+    } = process.env
+
+    /** Platform API call for products */
+    const response = await axios.get(`${API_HOST}/${API_TENANT}/v1/products/${event?.queryStringParameters?.id}`, {
+      auth: {
+        username: API_TENANT,
+        password: API_ACCESS_TOKEN,
+      },
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+        'Accept': 'application/vnd.api+json',
+        'User-Agent': 'SHIFT Commerce mm astro demo',
+      },
+    })
+    console.log('handler', { response })
+
+    return { 
+      statusCode: 200,
+      body: JSON.stringify({ data: transformProduct(response?.data?.data) })
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
+
