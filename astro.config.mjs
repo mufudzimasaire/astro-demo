068d4c2f36a159d48931dae6044f152bcd225422
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2022-10-18 11:59:44 +0100

    Add initial get-products-list handler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify/edge-functions';
+import netlify from '@astrojs/netlify/functions';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	adapter: netlify()
--- netlify/functions/get-product-list.ts
+++ netlify/functions/get-product-list.ts
@@ -1,25 +1,46 @@
----
-import HeaderLink from './HeaderLink.astro';
-import { SITE_TITLE } from '../config';
----
+const axios = require('axios');
 
-<header>
-	<h2>
-		{SITE_TITLE}
-	</h2>
-	<nav>
-		<HeaderLink href="/">Home</HeaderLink>
-		<HeaderLink href="/blog">Blog</HeaderLink>
-		<HeaderLink href="/about">About</HeaderLink>
-		<HeaderLink href="https://twitter.com/astrodotbuild" target="_blank">Twitter</HeaderLink>
-		<HeaderLink href="https://github.com/withastro/astro" target="_blank">GitHub</HeaderLink>
-	</nav>
-</header>
-<style>
-	header {
-		margin: 0em 0 2em;
-	}
-	h2 {
-		margin: 0.5em 0;
-	}
-</style>
+/**
+ * Products API Endpoint
+ *
+ * Purpose: Fetch first 25 products from the Platform API
+ *
+ * Example:
+ * ```
+ * fetch('/.netlify/functions/get-product-list')
+ * ```
+ */
+export const handler = async () => {
+  try {
+    const {
+      API_TENANT,
+      API_HOST,
+      API_ACCESS_TOKEN
+    } = process.env
+
+    /** Platform API call for products */
+    const response = await axios.get(`${API_HOST}/${API_TENANT}/v2/products?include=variants&fields[variants]=reference,sku&fields[products]=title,reference`, {
+      auth: {
+        username: API_TENANT,
+        password: API_ACCESS_TOKEN,
+      },
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+        'Accept': 'application/vnd.api+json',
+        'User-Agent': 'SHIFT Commerce mm astro demo',
+      },
+    })
+
+    return { 
+      statusCode: 200,
+      body: response?.data?.data ? JSON.stringify({ data: response?.data?.data }) : []
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
+
